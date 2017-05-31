7bfab56a628d1c86a14936928f42e8454bd9d4c8
Matt Wagner <m.wagner@dwdiesel.com>
2017-05-31 10:50:34 -0400

    Add basic console app to insert Samurai data

---
+++ src/SomeUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+using SamuraiAppCore.Data;
+using SamuraiAppCore.Domain;
+
+namespace SomeUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertSamurai();
+        }
+
+        private static void InsertSamurai()
+        {
+            var samurai = new Samurai { Name = "Matt" };
+            using (var context=new SamuraiContext())
+            {
+                context.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
+        }
+    }
+}
